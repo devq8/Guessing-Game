0a4f9ce4741a5fbfdd02dfa4c825448011a9bda0
Khaled Alghanim <al.ghanim@me.com>
2023-03-19 17:58:04 +0300

    CSS and HTML task is done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Guessing Game</h1>
       </header>
+      <div className='App-body'>
+        <h4>Find a number by entering it below</h4>
+        <input type='number' placeholder='Enter a number from 1 to 100' className='form-control number-input'/>
+        <button type='submit' className='btn btn-primary buttons'>Submit</button>
+        <div className='row-buttons'>
+          <button className='btn btn-warning buttons'>Surrender</button>
+          <button className='btn btn-warning buttons'>Hint</button>
+        </div>
+        <p>You have only 5 attempts, use it wisely 😉</p>
+      </div>
     </div>
   );
 }
